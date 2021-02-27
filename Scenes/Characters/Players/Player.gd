19379133e8854089ec43ee96435a1b8d47db7e58
Andrea Romagnoli <romagnoli.music@gmail.com>
2021-02-28 00:58:52 +0100

    Add player movement with camera

--- Scenes/Characters/Players/Player.gd
+++ Scenes/Characters/Players/Player.gd
@@ -9,26 +9,52 @@ const BLEND_TO_IDLE = 0.1
 var motion = Vector3()
 var movement_state = 0  # idle:0, run:1
 
+export var mouse_sensitivity = 1200
+
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
 
 func _physics_process(_delta):
 	move()
 	animate()
 
 
+func _input(event):
+	if event is InputEventMouseMotion:
+		rotation = h_camera_rotation(-event.relative.x/mouse_sensitivity)
+		$Camera.rotation = v_camera_rotation(-event.relative.y/mouse_sensitivity)
+
+
 func move():
-	var x = Input.get_action_strength("forward") - Input.get_action_strength("back")
-	var z = Input.get_action_strength("right") - Input.get_action_strength("left")
+	var movement_2d_dir = get_2d_movement_dir()
+	var direction = Vector3.ZERO
+	var camera_xform = $Camera.global_transform
 	
-	motion = Vector3(x,0,z)
+	direction -= camera_xform.basis.z.normalized() * movement_2d_dir.x
+	direction += camera_xform.basis.x.normalized() * movement_2d_dir.y
+	direction.y = 0
 	
-	if not motion == Vector3(0,0,0):
-		face_forward(x,z)
-	
+	motion = direction
+
 	move_and_slide(motion * SPEED, UP)
 
 
-func face_forward(x, z):
-	rotation.y = atan2(x,z)
+func get_2d_movement_dir():
+	var x = Input.get_action_strength("forward") - Input.get_action_strength("back")
+	var y = Input.get_action_strength("right") - Input.get_action_strength("left")
+	
+	var movement_dir = Vector2(x,y)
+	
+	if not movement_dir == Vector2.ZERO:
+		face_forward(x,y)
+	
+	return movement_dir.normalized()
+
+
+func face_forward(x, y):
+	$Armature.rotation.y = atan2(x,y) - PI/2
 
 
 func animate():
@@ -39,3 +65,14 @@ func animate():
 	movement_state = clamp(movement_state, 0, 1)
 
 	$Armature/AnimationTree.set("parameters/Move/blend_amount", movement_state)
+
+
+func h_camera_rotation(camera_rotation):
+	return rotation + Vector3(0,camera_rotation,0)
+
+
+func v_camera_rotation(camera_rotation):
+	var rot = $Camera.rotation + Vector3(camera_rotation,0,0)
+	rot.x = clamp(rot.x, -PI/8, PI/20)
+	return rot
+
