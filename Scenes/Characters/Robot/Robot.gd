8979c873a4b38bcefab8db421fe4a9758874ad38
Andrea Romagnoli <romagnoli.music@gmail.com>
2021-03-07 20:00:25 +0100

    Robots lose lives

--- Scenes/Characters/Robot/Robot.gd
+++ Scenes/Characters/Robot/Robot.gd
@@ -7,3 +7,9 @@ func _ready():
 func _physics_process(delta):
 	if $RayCast.is_colliding():
 		try_to_fire(INF)
+
+
+func update_lives():
+	if lives > 0:
+		var animation = $Lives.get_child(0).get_child(0)
+		animation.play("Lose Life")
