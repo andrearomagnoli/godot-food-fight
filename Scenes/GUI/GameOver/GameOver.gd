47d275e8466b239a015abb81ef09abf3c3e3bdcc
Andrea Romagnoli <romagnoli.music@gmail.com>
2021-03-07 19:30:44 +0100

    Create game over and victory scenes

---
+++ Scenes/GUI/GameOver/GameOver.gd
@@ -0,0 +1,13 @@
+extends Control
+
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+
+
+func _on_RestartButton_pressed():
+	get_tree().change_scene("res://Level/Level1.tscn")
+
+
+func _on_QuitButton_pressed():
+	get_tree().quit()
